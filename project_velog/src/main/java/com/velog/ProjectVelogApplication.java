4e4e6c45a42df0864ad6559760da3eb0d4b66481
over0212 <liberty0212@naver.com>
2021-11-17 18:58:01 +0900

    2021-11-17 day01(프로젝트 초기 설정)

---
+++ project_velog/src/main/java/com/velog/ProjectVelogApplication.java
@@ -0,0 +1,13 @@
+package com.velog;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjectVelogApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProjectVelogApplication.class, args);
+	}
+
+}
