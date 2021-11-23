cc5e1df43cef9df227e949c6f2779f888a066474
DESKTOP-7EAO6US\NOTE <coqja2013@naver.com>
2021-11-23 21:05:13 +0900

    Leo 개인정보 작업중

---
+++ front/js/mypage.js
@@ -0,0 +1,13 @@
+const switch_off = document.querySelectorAll('.switch_off');
+const circle = document.querySelectorAll('.circle');
+
+// 스위치 온오프 기능
+
+
+switch_off[0].onclick = () => {
+    //switch_off[0].style.background = '#12b886';
+}
+
+switch_off[1].onclick = () => {
+    switch_off[1].style.background = '#12b886';
+}
