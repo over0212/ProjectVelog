bf698f69a2b7d72cda39d8ac0d6bb07aadf6a504
LAPTOP-Q9OSK4EJ\정순화 <nanee0706@gmail.com>
2021-11-25 17:16:35 +0900

    Nana auth sign_in 도전

--- project_velog/src/main/java/com/velog/config/SecurityConfig.java
+++ project_velog/src/main/java/com/velog/config/SecurityConfig.java
@@ -1,26 +1,40 @@
 package com.velog.config;
 
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 
 @EnableWebSecurity
 @Configuration
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 	
+//	@Override
+//	protected void configure(HttpSecurity http) throws Exception {	
+//		http.csrf().disable();
+//		
+//		http.authorizeRequests()
+//			.anyRequest()
+//			.permitAll();	
+//	}
+	
 	@Override
-	protected void configure(HttpSecurity http) throws Exception {
-		
-		http.csrf().disable();
-		
-		http.authorizeRequests()
-
+	public void configure(HttpSecurity httpSecurity) throws Exception {
+		httpSecurity.csrf().disable()
+			.authorizeRequests()
+			.antMatchers("/api/auth/**")
+			.permitAll()
 			.anyRequest()
-			.permitAll();
-			
-		
+			.authenticated();
+	}
+	
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
 	}
 
 }
