4732f321b472fe4e67fbb2e80af9d4866bb5a67f
over0212 <liberty0212@naver.com>
2021-12-01 16:58:29 +0900

    aiden

--- project_velog/src/main/java/com/velog/web/controller/PageController.java
+++ project_velog/src/main/java/com/velog/web/controller/PageController.java
@@ -2,14 +2,10 @@ package com.velog.web.controller;
 
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import com.velog.config.auth.PrincipalDetails;
-import com.velog.web.service.UserService;
 
 import lombok.RequiredArgsConstructor;
 
