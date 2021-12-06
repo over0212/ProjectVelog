64986045a48e2dde3959ce437673b123774b4b9d
DESKTOP-7EAO6US\NOTE <coqja2013@naver.com>
2021-12-07 00:56:01 +0900

    초

--- project_velog/src/main/java/com/velog/web/service/ImgService.java
+++
@@ -1,9 +0,0 @@
-package com.velog.web.service;
-
-import com.velog.web.model.dto.mypage.MypageDto;
-
-public interface ImgService {
-
-	public String fileUpload(String username,MypageDto mypageDto);
-	public int mypageinsert(MypageDto mypageDto);
-}
