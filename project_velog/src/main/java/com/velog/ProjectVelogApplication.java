317e172f6457090b96d310ab35e55188f698c48f
DESKTOP-7EAO6US\NOTE <coqja2013@naver.com>
2021-12-09 17:37:38 +0900

    Leo

--- project_velog/src/main/java/com/velog/ProjectVelogApplication.java
+++ project_velog/src/main/java/com/velog/ProjectVelogApplication.java
@@ -2,12 +2,21 @@ package com.velog;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ProjectVelogApplication {
-
+public class ProjectVelogApplication extends SpringBootServletInitializer{
+	
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder builder) {
+		return builder.sources(ProjectVelogApplication.class);
+	}
+	
 	public static void main(String[] args) {
 		SpringApplication.run(ProjectVelogApplication.class, args);
+		
+		
 	}
 
 }
